b66acfc8463192602c9d316dc89b3b11b9410662
John Rossi <DACOOK1983@gamil.com>
2020-05-11 21:21:27 -0400

    all dependencies added, app functional. beggining project

--- darkmode/src/App.js
+++ darkmode/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import ReactDOM from "react-dom";
+import axios from "axios";
+
+import Charts from "./components/Charts"
+import Navbar from "./components/Navbar";
+
 
 function App() {
+
+   const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+      )
+      .then(res => setCoinData(res.data))
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Charts coinData={coinData} />
     </div>
   );
 }
